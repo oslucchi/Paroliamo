d514e9aade3c620468e9235eb0a646d779993df5
Osvaldo Lucchini <osvaldo.lucchini@gmail.com>
2025-07-03 21:44:37 +0200

    first running version built for mobile

--- src/components/Matrix.tsx
+++ src/components/Matrix.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import { italianAlphabetProbabilities } from '../data/italianAlphabet';
-import { generateLetter } from '../utils/letterGenerator';
-import { Dimensions } from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
+import {italianAlphabetProbabilities} from '../data/italianAlphabet';
+import {generateLetter} from '../utils/letterGenerator';
+import {Dimensions} from 'react-native';
 
 interface MatrixProps {
   rows?: number;
@@ -17,10 +17,9 @@ const Matrix: React.FC<MatrixProps> = ({
   visible,
   matrix,
 }) => {
-
-    const screenWidth = Dimensions.get('window').width;
-    const totalSpacing = 8 * cols; // margin between cells
-    const cellSize = (screenWidth - totalSpacing - 32) / cols; // 32 for horizontal padding
+  const screenWidth = Dimensions.get('window').width;
+  const totalSpacing = 8 * cols; // margin between cells
+  const cellSize = (screenWidth - totalSpacing - 32) / cols; // 32 for horizontal padding
 
   if (!visible) return null;
 
@@ -29,10 +28,17 @@ const Matrix: React.FC<MatrixProps> = ({
       {matrix.map((row, rowIndex) => (
         <View style={styles.row} key={`row-${rowIndex}`}>
           {row.map((letter, colIndex) => (
-            <View style={[styles.cell, { width: cellSize, height: cellSize }]} key={`cell-${rowIndex}-${colIndex}`}>
-                <Text style={{fontSize: cellSize * 0.6, fontWeight: 'bold'}}>
-                    {visible ? letter : ' '}
-                </Text>
+            <View
+              style={[styles.cell, {width: cellSize, height: cellSize}]}
+              key={`cell-${rowIndex}-${colIndex}`}>
+              <Text
+                style={{
+                  fontSize: cellSize * 0.6,
+                  fontWeight: 'bold',
+                  color: '#c22200',
+                }}>
+                {visible ? letter : ' '}
+              </Text>
             </View>
           ))}
         </View>
@@ -44,23 +50,22 @@ const Matrix: React.FC<MatrixProps> = ({
 export default Matrix;
 
 const styles = StyleSheet.create({
-    container: {
-    marginVertical: 20,
-    alignItems: 'center',
-    paddingHorizontal: 16,
-    },
   row: {
     flexDirection: 'row',
   },
   cell: {
-    width: 50,
-    height: 50,
-    margin: 4,
+    flex: 1,
+    aspectRatio: 1,
+    margin: 2,
     justifyContent: 'center',
     alignItems: 'center',
+    backgroundColor: '#f5f5dc', // light beige
     borderWidth: 1,
-    borderColor: '#999',
-    borderRadius: 5,
-    backgroundColor: '#f5f5f5',
+    borderColor: '#d2b48c', // light brown
+  },
+  container: {
+    paddingHorizontal: 16,
+    paddingTop: 10,
+    alignItems: 'center',
   },
 });
